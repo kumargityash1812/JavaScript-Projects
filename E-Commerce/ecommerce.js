0b4f5ae9374710c7a05a301f7a415915818606f3
kumargityash1812 <117013662+kumargityash1812@users.noreply.github.com>
2022-11-04 12:52:49 +0530

    Update ecommerce.js

--- E-Commerce/ecommerce.js
+++ E-Commerce/ecommerce.js
@@ -3,6 +3,20 @@ const nav = document.getElementById("navbar");
 const closeBtn = document.getElementById("close");
 const cart=document.getElementById("cart1");
 const header = document.getElementById("header");
+  let mainImg=document.querySelector("#mainImg");
+        let smallImg=document.getElementsByClassName("small-img");
+        smallImg[0].addEventListener("click",()=>{
+            mainImg.src=smallImg[0].src;
+        });
+        smallImg[1].addEventListener("click", () => {
+                mainImg.src = smallImg[1].src;
+            });
+            smallImg[2].addEventListener("click", () => {
+                    mainImg.src = smallImg[2].src;
+                });
+                smallImg[3].addEventListener("click", () => {
+                        mainImg.src = smallImg[3].src;
+                    });
 if (bar) {
     bar.addEventListener("click", () => {
         nav.classList.add("active");
